53d5d549531974ecff62f169b37905bbbdb0b949
Kostiantyn Pidlisnyi <podlesny@outlook.com>
2021-02-06 18:21:38 +0200

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useRef } from "react";
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const inputFileRef = useRef(undefined);
+
+  const getUsersFromCsv = (result) => {
+    // do something to get users from string
+    return [
+      {
+        id: 1,
+        name: "Andrii",
+      },
+    ]; // [{ id: 1, name: "Greg"}];
+  };
+
+  const handleChange = () => {
+    const reader = new FileReader();
+
+    reader.onload = function () {
+      const users = getUsersFromCsv(reader.result); // ""
+      setUsers(users);
+    };
+
+    console.log(inputFileRef.current.files[0])
+    reader.readAsText(inputFileRef.current.files[0]);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        ref={inputFileRef}
+        style={{ display: "none" }}
+        type="file"
+        accept=".csv"
+        onChange={handleChange}
+      />
+
+      <button onClick={() => {
+        inputFileRef.current.click()
+      }}>Import users</button>
+
+      {users.map((user) => (
+        <div key={user.id}>
+          <p>{user.id}</p>
+          <p>{user.name}</p>
+        </div>
+      ))}
     </div>
   );
 }
